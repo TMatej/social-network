3516e42834763b068fcd7670597dba9e8ec2d804
Matej Turek <469107@mail.muni.cz>
2022-10-01 19:36:54 +0200

    [+] init DAL layer created with console app and nunit test app

---
+++ DataAccessLayer/DalUnitTests/EntityTests/UserTest.cs
@@ -0,0 +1,38 @@
+﻿using DataAccessLayer;
+using DataAccessLayer.Entity;
+using Microsoft.IdentityModel.Protocols;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DalUnitTests.EntityTests
+{
+    public class UserTest
+    {
+        [Test]
+        public void Test1()
+        {
+            var connectionString = ConfigurationManager.AppSettings["ConnectionString"];
+            if (connectionString == null)
+            {
+                Assert.Fail("Null connection string");
+            }
+
+            using (var db = new SocialNetworkDBContext(connectionString))
+            {
+                db.Database.EnsureCreated();
+
+                db.Users.Add(new User { Username = "lokomotiva123", PrimaryEmail = "cokoloko@gmail.com", PasswordHash = "0123456789abcde0" });
+                db.SaveChanges();
+
+                var user = db.Users.FirstOrDefault();
+                Assert.NotNull(user);
+                Assert.Equals(user.Id, 1);
+            }
+            
+        }
+    }
+}
---
+++ DataAccessLayer/DataAccessLayer/SocialNetworkDBContext.cs
@@ -0,0 +1,35 @@
+﻿using DataAccessLayer.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer
+{
+    public class SocialNetworkDBContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        
+        private string connectionString { get; set; }
+
+        public SocialNetworkDBContext(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(connectionString);
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<User>()
+                .HasIndex(u => u.Username)
+                .IsUnique();
+        }
+    }
+}
