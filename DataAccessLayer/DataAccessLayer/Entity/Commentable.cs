b5309d7b3feb552da52dd601daaea254b2f6e5a8
Michal Čížek <cizek@inqool.cz>
2022-10-02 19:52:37 +0200

    added post and commantable entities

---
+++ DataAccessLayer/DataAccessLayer/Entity/Commentable.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccessLayer.Entity
+{
+    [Table("Commentable")]
+    public class Commentable
+    {
+        [Key]
+        public int Id { get; set; }
+    }
+}
---
+++ DataAccessLayer/DataAccessLayer/Entity/Post.cs
@@ -0,0 +1,31 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccessLayer.Entity
+{
+    [Table("Post")]
+    internal class Post: Commentable
+    {
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public User User { get; set; }
+
+        [Required]
+        public int PostableId { get; set; }
+
+        [Required]
+        [MinLength(4)]
+        [MaxLength(64)]
+        public string Title { get; set; }
+    
+        [Required]
+        [MinLength(4)]
+        [MaxLength(256)]
+        public string Content { get; set; }
+
+        [Required]
+        public DateTime CreatedDate { get; set; }
+    }
+}
