834d9fd979b21bfa0c671f56fbcbbffd869d799f
Michal Čížek <cizek@inqool.cz>
2022-10-02 18:51:45 +0200

    added group entities

---
+++ DataAccessLayer/DataAccessLayer/Entity/Group.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccessLayer.Entity
+{
+    [Table("Group")]
+    public class Group : Postable
+    {
+        [MinLength(4)]
+        [MaxLength(64)]
+        [Required]
+        public string Name { get; set; }
+
+        [MaxLength(255)]
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+    }
+}
