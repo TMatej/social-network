7de52c442b7bd7818dcda31cc9875347e38b7bcc
Daniel Pivoňka <xpivonk1@fi.muni.cz>
2022-10-02 18:34:37 +0200

    add attachmnet, conversation and message

---
+++ DataAccessLayer/DataAccessLayer/Entity/Attachment.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataAccessLayer.Entity
+{
+    [Table("Attachment")]
+    class Attachment
+    {
+        [Key]
+        [Column("AttachmentId")]
+        public int ID { get; set; }
+
+        [Column("MessageId")]
+        [Required]
+        public int MessageID { get; set; }
+        [Required]
+        public Message Message { get; set; }
+        [Column("Url")]
+        [Required]
+        public string Url { get; set; }
+    }
+}
