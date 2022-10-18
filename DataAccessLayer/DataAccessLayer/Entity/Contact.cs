44ba21583de786c11fa6084bfae96b51739fbf3a
Matej Turek <469107@mail.muni.cz>
2022-10-19 00:36:02 +0200

    [+] Many-To-Many classes for Contact and ConversationParticipant created

---
+++ DataAccessLayer/DataAccessLayer/Entity/Contact.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata.Internal;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccessLayer.Entity
+{
+    public class Contact
+    {
+        [Required]
+        public int User1Id { get; set; }
+
+        [ForeignKey(nameof(User1Id))]
+        public User User1 { get; set; }
+
+        [Required]
+        public int User2Id { get; set; }
+
+        [ForeignKey(nameof(User2Id))]
+        public User User2 { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+    }
+}
---
+++ DataAccessLayer/DataAccessLayer/Entity/ConversationParticipant.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace DataAccessLayer.Entity
+{
+    public class ConversationParticipant
+    {
+        [Required]
+        public int UserId { get; set; }
+
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; }
+
+        [Required]
+        public int ConversationId { get; set; }
+
+        [ForeignKey(nameof(ConversationId))]
+        public Conversation Conversation { get; set; }
+
+        [Required]
+        public DateTime CreatedAt { get; set; }
+    }
+}
