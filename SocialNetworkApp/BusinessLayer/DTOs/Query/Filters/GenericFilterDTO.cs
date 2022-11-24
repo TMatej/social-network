b438d83d1928e8f020823a495922a9e36b1fd0c6
Matej Turek <469107@mail.muni.cz>
2022-11-24 08:27:45 +0100

    [+] Added initial values

--- SocialNetworkApp/BusinessLayer/DTOs/Query/Filters/GenericFilterDTO.cs
+++ SocialNetworkApp/BusinessLayer/DTOs/Query/Filters/GenericFilterDTO.cs
@@ -4,8 +4,8 @@ namespace BusinessLayer.DTOs.Query.Filters
 {
     public class GenericFilterDTO /* maybe record */
     {
-        public int? RequestedPageNumber { get; set; }   // For IQuery.Page()
-        public int? RequestedPageSize { get; set; }     // For IQuery.Page()
-        public IEnumerable<string> IncludeParameters { get; set; }    // For IQuery.Include()
+        public int? RequestedPageNumber { get; set; } = null;  // For IQuery.Page()
+        public int? RequestedPageSize { get; set; } = null;   // For IQuery.Page()
+        public IEnumerable<string> IncludeParameters { get; set; } = new List<string>();   // For IQuery.Include()
     }
 }
