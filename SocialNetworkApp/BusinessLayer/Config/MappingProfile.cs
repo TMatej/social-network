bdd02a51737de6eb52da449e93c836842dc9890a
Matej Turek <469107@mail.muni.cz>
2022-11-26 12:02:18 +0100

    [~] Sorting of components to register in Mapping profile

--- SocialNetworkApp/BusinessLayer/Config/MappingProfile.cs
+++ SocialNetworkApp/BusinessLayer/Config/MappingProfile.cs
@@ -1,3 +1,4 @@
+using BusinessLayer.DTOs.Comment;
 using BusinessLayer.DTOs.Photo;
 using BusinessLayer.DTOs.Post;
 using BusinessLayer.DTOs.Profile;
@@ -12,15 +13,28 @@ namespace BusinessLayer.Config
 
         public MappingProfile()
         {
+            /* Gallery */
             CreateMap<Gallery, GalleryBasicRepresentDTO>();
             CreateMap<GalleryCreateDTO, Gallery>();
-            CreateMap<Photo, PhotoInsertDTO>().ReverseMap();
-            CreateMap<Photo, PhotoRepresentDTO>().ReverseMap();
-            CreateMap<DataAccessLayer.Entity.Profile, ProfileBasicRepresentDTO>().ReverseMap();
             CreateMap<Gallery, GalleryRepresentDTO>()
                 .ForMember(dest => dest.Profile,
                     opt => opt.MapFrom(src => src.Profile));
-            CreateMap<PostCreateDTO, Post>().ReverseMap();
+            
+            /* Photo */
+            CreateMap<Photo, PhotoInsertDTO>().ReverseMap();
+            CreateMap<Photo, PhotoRepresentDTO>().ReverseMap();
+            
+            /* Comment */
+            CreateMap<Comment, CommentCreateDTO>().ReverseMap();
+            CreateMap<Comment, CommentEditDTO>().ReverseMap();
+            CreateMap<Comment, CommentRepresentDTO>().ReverseMap();
+            CreateMap<Comment, CommentBasicRepresentDTO>().ReverseMap();
+            
+            /* Profile */
+            CreateMap<DataAccessLayer.Entity.Profile, ProfileBasicRepresentDTO>().ReverseMap();      
+            
+            /* Post */
+            CreateMap<PostCreateDTO, Post>().ReverseMap();  
         }
     }
 }
