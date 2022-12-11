03b1272c7ee0fc56bed3a72bd8d46c3e65817b2d
Matej Turek <469107@mail.muni.cz>
2022-12-11 22:21:32 +0100

    Added skeleton of controllers necessary for profile of client

---
+++ SocialNetworkApp/PresentationLayer/Controllers/CommentsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using PresentationLayer.Models;
+
+namespace PresentationLayer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class CommentsController : ControllerBase
+    {
+        //GET /comments?entityId={entityId}&page={page}&size={size} - vrati komentare pro commentable entitu s entityId se strankovanim
+        [HttpGet]
+        public IActionResult GetCommentsOfCommentableEntity(int entityId, int page, int size)
+        {
+            return Ok();
+        }
+
+        //POST /comments?entityId={entityId} - prida comment pod libovolnou commantable entitu s entityId
+        [HttpPost]
+        public IActionResult AddCommentToCommentableEntity(int entityId)
+        {
+            return Ok();
+        }
+
+        //DELETE /comments/{commentId} - odstrani komentar (kontrola kdo vytvoril)
+        [HttpDelete("{commentId}")]
+        public IActionResult DeleteComment(int commentId)
+        {
+            return Ok();
+        }
+    }
+}
---
+++ SocialNetworkApp/PresentationLayer/Controllers/IndexController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PresentationLayer.Controllers
+{
+    [ApiController]
+    [Route("")]
+    public class IndexController : ControllerBase
+    {
+        public IndexController()
+        {
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok("Hello, this is index! Our page is under the development rn, please be patient. It will be here in no time :D");
+        }
+    }
+}
