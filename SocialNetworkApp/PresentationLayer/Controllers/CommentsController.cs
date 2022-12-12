f73f890e109daa0e432c8799e9485e4312574999
Daniel Pivoňka <xpivonk1@fi.muni.cz>
2022-12-12 22:34:55 +0100

    comments

--- SocialNetworkApp/PresentationLayer/Controllers/CommentsController.cs
+++ SocialNetworkApp/PresentationLayer/Controllers/CommentsController.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using BusinessLayer.Contracts;
+using BusinessLayer.DTOs.Comment;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using PresentationLayer.Models;
 
@@ -8,24 +10,33 @@ namespace PresentationLayer.Controllers
     [Route("[controller]")]
     public class CommentsController : ControllerBase
     {
+        readonly ICommentService commentService;
+        public CommentsController(ICommentService commentService) {
+            this.commentService = commentService;
+        }
         //GET /comments?entityId={entityId}&page={page}&size={size} - vrati komentare pro commentable entitu s entityId se strankovanim
-        [HttpGet]
+        [HttpGet("?entityId={entityId}&page={page}&size={size}")]
         public IActionResult GetCommentsOfCommentableEntity(int entityId, int page, int size)
         {
-            return Ok();
+            var comments = commentService.getCommentsForEntity(entityId, page, size);
+            return Ok(comments);
         }
 
         //POST /comments?entityId={entityId} - prida comment pod libovolnou commantable entitu s entityId
+        //TODO permissions
         [HttpPost]
-        public IActionResult AddCommentToCommentableEntity(int entityId)
+        public IActionResult AddCommentToCommentableEntity(int entityId, [FromBody] CommentCreateDTO commentCreateDTO)
         {
+            commentService.AddComment(commentCreateDTO);
             return Ok();
         }
 
         //DELETE /comments/{commentId} - odstrani komentar (kontrola kdo vytvoril)
+        //TODO permission check
         [HttpDelete("{commentId}")]
         public IActionResult DeleteComment(int commentId)
         {
+            commentService.Delete(commentId);
             return Ok();
         }
     }
