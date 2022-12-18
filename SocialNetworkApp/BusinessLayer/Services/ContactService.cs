7740f670a752871f0b6e5726a59a4900302ab851
Daniel Pivonka <xpivonk1@fi.muni.cz>
2022-12-18 21:29:56 +0100

    add friemd controller

--- SocialNetworkApp/BusinessLayer/Services/ContactService.cs
+++ SocialNetworkApp/BusinessLayer/Services/ContactService.cs
@@ -1,5 +1,7 @@
 ﻿using BusinessLayer.Contracts;
+using DataAccessLayer.Entity;
 using DataAccessLayer.Entity.JoinEntity;
+using Infrastructure.Query;
 using Infrastructure.Repository;
 using Infrastructure.UnitOfWork;
 
@@ -7,11 +9,13 @@ namespace BusinessLayer.Services
 {
     public class ContactService : GenericService<Contact>, IContactService
     {
-        public ContactService(IRepository<Contact> repository, IUnitOfWork uow) : base(repository, uow)
+        IQuery<Contact> query;
+        public ContactService(IRepository<Contact> repository, IQuery<Contact> query, IUnitOfWork uow) : base(repository, uow)
         {
+            this.query = query;
         }
 
-        public void addContact(int userId, int addedContactUserId)
+        public void AddContact(int userId, int addedContactUserId)
         {
             var contact = new Contact()
             {
@@ -19,7 +23,21 @@ namespace BusinessLayer.Services
                 User2Id = addedContactUserId,
             };
 
-            this.Insert(contact);
+            Insert(contact);
+        }
+
+
+        public IEnumerable<User> GetContacts(int userId)
+        {
+            var contacts = query.Where<int>(id => id == userId, nameof(Contact.User1Id)).Include(nameof(Contact.User2)).Execute().Items;
+            return contacts.Select(contact => contact.User2);
+        }
+
+        public void RemoveContact(int user1Id, int user2Id)
+        {
+            var contact = query.Where<int>(id => id == user1Id, nameof(Contact.User1Id)).Where<int>(id => id == user2Id, nameof(Contact.User2Id)).Execute().Items.Single();
+            Delete(contact);
+
         }
     }
 }
---
+++ SocialNetworkApp/PresentationLayer/Controllers/ContactController.cs
@@ -0,0 +1,43 @@
+﻿using BusinessLayer.DTOs.Search;
+using BusinessLayer.Facades.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using PresentationLayer.Models;
+
+namespace PresentationLayer.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ContactController : ControllerBase
+    {
+        readonly IContactFacade contactFacade;
+
+        public ContactController(IContactFacade contactFacade)
+        {
+            this.contactFacade = contactFacade;
+        }
+
+        //GET /contact/{id}/
+        [HttpGet("{id}")]
+        public IActionResult GetContact(int id)
+        {
+            var users = contactFacade.GetContacts(id);
+            return Ok(users);
+        }
+
+        //PUT /contact/{id}/friends?targetUserId={targetUserId}
+        [HttpPut("{id}/friends")]
+        public IActionResult AddContact(int id, int targetUserId)
+        {
+            contactFacade.AddContact(id, targetUserId);
+            return Ok();
+        }
+
+        //DELETE /contact/{id}/friends?targetUserId={targetUserId}
+        [HttpDelete("{id}/friends")]
+        public IActionResult DeleteFriend(int id, int targetUserId)
+        {
+            contactFacade.RemoveContact(id, targetUserId);
+            return Ok();
+        }
+    }
+}
