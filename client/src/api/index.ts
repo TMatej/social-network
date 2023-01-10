3132ab9f3f7d89a315cedce2473736547f3805c3
Matej Turek <469107@mail.muni.cz>
2023-01-10 12:16:50 +0100

    removed slash at the end of the base URL

--- client/src/api/index.ts
+++ client/src/api/index.ts
@@ -1,3 +1,3 @@
 export const baseUrl = import.meta.env.DEV
   ? "http://localhost:5000"
-  : "https://social-network-application-server.dyn.cloud.e-infra.cz/";
+  : "https://social-network-application-server.dyn.cloud.e-infra.cz";
